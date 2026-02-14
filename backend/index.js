70309e4d4169f25c2fbf889ebd96a384e39ea3d9
Dharani <dharani@Dharanis-MacBook-Air.local>
2026-02-14 14:26:07 +0530

    User manager app

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const conn = require('./Models/db.js')
+const UserRoutes = require('./Routes/UserRoutes.js')
+
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/user', UserRoutes)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`server started at port: ${PORT}`);
+    conn();
+}
+)
+
