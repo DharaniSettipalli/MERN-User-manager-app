f368b7ee69291fe2cf09ee38dd3532540c7dde48
DharaniSettipalli <47620993+DharaniSettipalli@users.noreply.github.com>
2026-02-14 15:08:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,10 @@ app.use('/user', UserRoutes)
 
 const PORT = process.env.PORT || 8080
 
+app.get('/', (req, res) => {
+    res.send('Hello from server')
+})
+
 app.listen(PORT, () => {
     console.log(`server started at port: ${PORT}`);
     conn();
